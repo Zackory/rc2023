dd307ce1edc600f30c5ad3b55d072fb46cf9b08b
Zackory Erickson <zackory@zackorys-air.wifi.local.cmu.edu>
2023-01-15 18:22:04 -0500

    Transformations

---
+++ stretch_python/06_transformations.py
@@ -0,0 +1,22 @@
+import rospy, tf
+import stretch_body.robot
+robot = stretch_body.robot.Robot()
+robot.startup()
+
+listener = tf.TransformListener()
+from_frame_rel = 'base_link'
+to_frame_rel = 'link_grasp_center'
+
+rospy.sleep(1.0)
+rate = rospy.Rate(1)
+
+robot.arm.move_to(0.5)
+robot.push_command()
+
+while not rospy.is_shutdown():
+    try:
+        translation, rotation = listener.lookupTransform(to_frame_rel, from_frame_rel, rospy.Time(0))
+        rospy.loginfo('The pose of target frame %s with respect to %s is: \n %s, %s', to_frame_rel, from_frame_rel, translation, rotation)
+    except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+        continue
+    rate.sleep()
